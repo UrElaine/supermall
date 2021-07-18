c6b7a619696e85b988593bebbd64b0e7ba7a0694
UrElaine <1432936995@qq.com>
2021-07-18 10:57:15 +0800

    目录结构

---
+++ src/views/home/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMutiData} from 'network/home'
+
+export default {
+  name: "Home",
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  //生命周期函数
+  created() {
+    //1.请求数据
+    getHomeMutiData().then(res => {
+      //res是局部变量，函数执行完之后就会从内存中回收
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
+
+}
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+}
+</style>
