e1c8ef88054e8e37ee740e2123a13f89f76f8e2e
UrElaine <1432936995@qq.com>
2021-07-18 21:05:50 +0800

    轮播图 推荐

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,19 +3,27 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from "./childComps/HomeSwiper"
+import RecommendView from './childComps/RecommendView'
+
+
 import {getHomeMutiData} from 'network/home'
 
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper,
+    RecommendView
   },
-  data() {
+  data(){
     return {
       banners: [],
       recommends: []
